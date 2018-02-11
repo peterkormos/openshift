af50dfe2f67c08dead33ad381fd6989424a2d4c4
Peter Kormos <msz.kormospeter@gmail.com>
2018-02-11 21:54:29 +0100

    nevlap atdolgozas

--- src/main/java/servlet/RegistrationServlet.java
+++ src/main/java/servlet/RegistrationServlet.java
@@ -59,7 +59,7 @@ import tools.InitDB;
 
 public class RegistrationServlet extends HttpServlet
 {
-  public String VERSION = "2018.01.31.";
+  public String VERSION = "2018.02.11.";
   public static Logger logger = Logger.getLogger(RegistrationServlet.class);
 
   Map<String, ResourceBundle> languages = new HashMap<String, ResourceBundle>(); // key: HU, EN, ...
@@ -1943,7 +1943,7 @@ public class RegistrationServlet extends HttpServlet
 		      .replaceAll("__MODEL_IDENTIFICATION__", model.identification).replaceAll("__MODEL_PRODUCER__", model.producer)
 		      .replaceAll("__MODEL_COMMENT__", model.comment).replaceAll("__GLUED_TO_BASE__", model.gluedToBase ?
 
-		              language.getString("yes") : language.getString("no")
+		              language.getString("yes")+"<br>yes" : language.getString("no")+"<br>no"
 
 		  // "<font color='#006600'>Alapra ragasztva</font>"
 		  // :
