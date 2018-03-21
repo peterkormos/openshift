1b51482dde161907a42333f3d02c361cff115f94
Kormos Péter <peter.kormos@ujvilag.gov.hu>
2018-03-21 10:42:56 +0100

    teljes név cimke átírás

--- src/main/java/servlet/RegistrationServlet.java
+++ src/main/java/servlet/RegistrationServlet.java
@@ -61,7 +61,7 @@ import tools.InitDB;
 
 public class RegistrationServlet extends HttpServlet
 {
-  public String VERSION = "2018.03.16.";
+  public String VERSION = "2018.03.21.";
   public static Logger logger = Logger.getLogger(RegistrationServlet.class);
 
   Map<String, ResourceBundle> languages = new HashMap<String, ResourceBundle>(); // key: HU, EN, ...
